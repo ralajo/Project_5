e7d2012c213cd1524273818371d17bd51b3e88d5
ralajo <71774252+ralajo@users.noreply.github.com>
2020-12-18 13:53:12 -0500

    Update FluffStuff.js

--- FluffStuff.js
+++ FluffStuff.js
@@ -1,202 +1,67 @@
 // JavaScript Document
 //Global Variables
 //Product Page Editing Fill Title
-function fillTitle(){
-	var x = document.getElementById("pfill");
-	var i = x.selectedIndex;
-	return document.getElementById("fillinsert").innerHTML = x.options[i].text;
-}
+let localStorageCount = 0;
 
-function pricequantChange(){
-	var x = document.getElementById("pquantity");
-	var i = x.selectedIndex;
-	return document.getElementById("pprice").innerHTML = x.options[i].text * 30;
-}
-
-let pillowArray = [];
-
-function CardTemplate (parentEl, shape, color, fill, quantity, price, image, name){
-	let divContainer = document.createElement("div");
-	divContainer.className = "card";
-	parentEl.appendChild(divContainer);
-	
-	let img = document.createElement("img");
-	img.src = image;
-	img.width = 200;
-	divContainer.appendChild(img);
-	
-	let divTextContainer = document.createElement("div");
-	divTextContainer.className = "text";
-	divContainer.appendChild(divTextContainer);
-	
-	let header = document.createElement("h2");
-	header.innerHTML = name;
-	divTextContainer.appendChild(header);
-	
-	let quant = document.createElement("p");
-	quant.innerHTML = quantity;
-	divTextContainer.appendChild(quant);
-	
-	let pricetag = document.createElement("p");
-	pricetag.innerHTML = price;
-	divContainer.appendChild(pricetag);
-	
-	let shapepil = document.createElement("p");
-	shapepil.innerHTML = shape;
-	divTextContainer.appendChild(shapepil);
-	
-	let colorpil = document.createElement("p");
-	colorpil.innerHTML = color;
-	divTextContainer.appendChild(colorpil);
-	
-	let fillpil = document.createElement("p");
-	shapepil.innerHTML = fill;
-	divTextContainer.appendChild(fillpil);
-	
-	return divContainer;
-	
-	let removebtn = document.createElement('button');
-	removebtn.textContent = 'remove!';
-	removebtn.onclick = function () {
-    const pillow = {
-		header: name,
-		quant: quantity,
-		img: image,
-		pricetag: price,
-		shapepil: shape,
-		colorpil: color,
-		fillpil: fill,
-    }
-    pillowArray.push(pillow);
-    const json = window.localStorage.setItem("pillowArray",JSON.stringify(pillow));
-  };
-  divContainer.appendChild(removebtn);
-
-  return divContainer;
-}
-
-  // new function to load dogs from localStorage - walk through this in exercise
-function onLoad() {
-  const pillowArray = localStorage.getItem("pillowArray");
-  if(pillowArray === null) {
-    return;
-  }
-  else {
-    const pillowArray = JSON.parse(window.localStorage.setItem("pillowArray",JSON.stringify(pillow)));
-    // End of Part 2
-    // CardTemplate(divDoggoContainer, savedDogs.header, savedDogs.body, savedDogs.imgUrl);
-    // End of Part 3
-    cartItems.forEach((pillow) => {
-      CardTemplate (divDoggoContainer, pillow.header, pillow.quant, pillow.img, pillow.shapepil, pillow.colorpil, pillow.fillpil, pillow.pricetag);
-      pillowArray.push(pillow);
-    });
-  return;
-  }
-}
-
-function createnewitem(containerEl) {
-	let shape = document.getElementById("pshape").innerHTML;
-	let color = document.getElementById("pcolor").innerHTML;
-	let fill = document.getElementById("pfill").value;
-	let price = document.getElementById("pprice").innerHTML;
-	let quantity = function(){return this.price/30};
-	let image=document.getElementByClassName("mainProductImage").innerHTML;
-	let name = function(){return this.shape + " in " + this.color + " with " + this.fill + " Filling";};
-    CardTemplate(containerEl, shape, fill, price, quantity, image, color, name)
-    }
-
-let aCreateRandom = document.getElementById("a-create-random");
-const divDoggoContainer = document.getElementById("div-doggos");
-aCreateRandom.onsubmit = function (e) {
-  e.preventDefault();
-   createnewitem(divDoggoContainer);
-};
-
-  // new function to remove dogs from localStorage
-let clearButton = document.getElementById('clear-session-btn');
-clearButton.onclick = function (e) {
-  localStorage.removeItem('myDogs');
-}
-/
-const pillow(shape,color,fill,quantity,price,image,name){
-	this.shape = shape;
-	this.color = color;
-	this.fill = fill;
-	this.quantity = quantity;
-	this.price = price;
-	this.image = image;
-	this.name = function(){return this.shape + " in " + this.color + " with " + this.fill + " Filling";};
-}
-// JavaScript Document
-/*var localStorageCount = 0;
-var shape = "";
-var color = "";
-var fill = "";
-var quantity = "";
-var price = "";
-var image = "";
 var addPillow = "";
-var cartfill = "";
-var subtotal= "";
-var totalcost= "";
-var tax ="";
+let cartfill = "";
+let subtotal= 0;
+let totalcost= "";
+let tax ="";
 
-function fillTitle(){
-	var x = document.getElementById("pfill");
-	var i = x.selectedIndex;
-	document.getElementById("fillinsert").innerHTML = x.options[i].text;
+//Product Page Stuff
+	//Change Title of Fill
+	function fillTitle(){
+		var x = document.getElementById("pfill");
+		var i = x.selectedIndex;
+		document.getElementById("fillinsert").innerHTML = x.options[i].text;
 }
-
-function pricequantChange(){
-	var x = document.getElementById("pquantity");
-	var i = x.selectedIndex;
-	document.getElementById("pprice").innerHTML = x.options[i].text * 30;
+	//Change The Price to reflect the selected Quantity
+	function pricequantChange(){
+		var x = document.getElementById("pquantity");
+		var i = x.selectedIndex;
+		document.getElementById("pprice").innerHTML = x.options[i].text * 30;
 }
-function Pillows(shape,color,fill,quantity,price,image,name){
-	this.shape = shape;
-	this.color = color;
-	this.fill = fill;
-	this.quantity = quantity;
-	this.price = price;
-	this.image = image;
-	this.name = function(){this.shape + "in" + this.color;};
-}
-function addToCart() {
-	shape = document.getElementById("pshape").innerHTML;
-	color = document.getElementById("pcolor").innerHTML;
-	fill = document.getElementById("fillinsert").innerHTML;
-	price = document.getElementById("pprice").innerHTML;
-	quantity = this.price/30;
-	image = document.getElementById("MHRBig");
-	
-	addPillow = new Pillows(shape,color,fill,quantity,price,image,name);
-	alert(this.quantity + " "+ this.name + " with "+ this.fill + " ADDED TO CART")
-	
-	window.localStorage.setItem(localStorageCount, JSON.stringify(addPillow));
-}
-
-function openCart(){
-	for (i = 0; i < lsc; i++) {
-		item = JSON.parse(window.localStorage.getItem(i));
-		var itemKey = window.localStorage.key(i);
-		if (item === null) {
-			break;
-		} else {
-			var quantityCount = document.getElementById("quantityCount");
-			quantityCount.innerText = item.quantity;
-			document.getElementById("quantityCount").style.visibility = "visible";
-
-			document.getElementById("cartTotalItems").innerHTML += item.quantity
-			document.getElementById("cartItems").innerHTML += 
-			document.getElementById("cartItemQuantity").innerHTML += item.quantity;
-			document.getElementById("cartItemFill").innerHTML += item.fill;
-			document.getElementById("cartPriceDetails").innerHTML += item.price;
-
-			updateSubtotal();
-			calcTax();
-			calcDiscount();
-			calcFinalTotal();
+//Full Site stuff
+//Setting up the array of pillows first as that's gonna be transfered
+	let pillowArray = [];
+	//Change Cart Quantity # & Info
+	//Add Item to Cart
+		//Set up Pillow in COnstructor Function! to establish it is a group that new things can be made of it - the capital P = constructor
+		function Pillow (shape,color,fill,quantity,price,image,name){
+			this.shape = shape;
+			this.color = color;
+			this.fill = fill;
+			this.quantity = quantity;
+			this.price = price;
+			this.image = image;
+			this.name = function(){return this.shape + " in " + this.color + " with " + this.fill + " Filling";};
 		}
-	}
-}
-*/
+		//Make it so that a new pillow is created and populated when the submit button is pressed on the product page.
+		function addToCart (e){
+			let shape = document.getElementById("pshape").innerHTML;
+			let color = document.getElementById("pcolor").innerHTML;
+			let fill = document.getElementById("pfill").value;
+			let price = document.getElementById("pprice").innerHTML;
+			let quantity = function(){return this.price/30};
+			let image = document.getElementByClassName("mainProductImage").innerHTML;
+			let name = function(){return this.shape + " in " + this.color + " with " + this.fill + " Filling";};
+			
+			new Pillow (shape,color,fill,quantity,price,image,name);
+			
+			let subtotal = document.getElementById("cartcount").innerHTML += this.quantity;
+
+			window.localStorage.setItem("pillowArray",JSON.stringify(product));
+			pillowArray.push(pillow);
+	
+			console.log(JSON.parse(window.localStorage.getItem("pillowArray",JSON.stringify(product))));
+		}
+			//functions dependent upon this imput
+			function getTotalCost(){
+				return document.getElementById("cartcount").innerHTML * 30;
+			}
+
+			function 
+    //CardTemplate (Pillow.shape, Pillow.color, Pillow.fill, Pillow.quantity, Pillow.price, Pillow.image, Pillow.name);
+    
+	//Remove Item from Cart
